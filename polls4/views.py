d1f7558c5659478022dacecaa9a3568543299717
Charles Severance <csev@umich.edu>
2018-09-02 09:56:26 -0400

    Get rid of numbered template folder.

--- polls4/views.py
+++ polls4/views.py
@@ -10,14 +10,14 @@ from django.template import loader
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls4/index.html', context)
+    return render(request, 'polls/index.html', context)
 
 def owner(request):
     return HttpResponse("Hello, world. Jane Instructor / 1ff1de77 is the polls owner.")
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls4/detail.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
